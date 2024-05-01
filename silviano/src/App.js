cbaf60281b0a2bd4fb6bac9d3e6f8197f8b3738d
guismarck <guismarck01@gmail.com>
2024-05-01 08:54:15 +0200

    cambios actuales

--- silviano/src/App.js
+++ silviano/src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
-import "primereact/resources/primereact.main.css";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import "primeicons.css";
-import './App.css';
+// import "primereact/resources/themes/lara-light-cyan/theme.css";
+// import "primereact/resources/primereact.main.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "primeicons.css";
+// import './App.css';
+import ListarGrados from "./Componetes/controllers/ListarGrados";
 
 function App() {
+  console.log('Hi broooo')
   return (
 
-
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/"> en esora de home 
-   
-        </Route>
-      </Switch>
-      
-   <div>
-     <h3>Probado la aplicacion</h3>
-   </div>
-
+      <Routes>
+        <Route path="/" element={<h1>HELLO MOTO</h1>}/>
+        <Route path="/grados" element={<ListarGrados/>}/>
+          
+      </Routes>
     </BrowserRouter>
 
   );
