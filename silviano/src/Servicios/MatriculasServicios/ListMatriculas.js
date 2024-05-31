4654bda9c17fc21f1df7dfd6f4ded2f76d949e38
Guismark <guismarck01@gmail.com>
2024-05-31 07:51:57 +0200

    cambioss en listado de matriculas

--- silviano/src/Servicios/MatriculasServicios/ListMatriculas.js
+++ silviano/src/Servicios/MatriculasServicios/ListMatriculas.js
@@ -7,46 +7,42 @@ import settings from '../../../../silviano/src/settings.json';
 import axios from 'axios';
 //import BuscarEstudiantes from '../../../Servicios/EstudiantesServicios/BuscarEstudiantes';
 
-
 export default function ListMatriculas() {
     const urlMatricula = `${settings.api.baseUrl}/matriculas`;
-    const urlBase = `${settings.api.baseUrl}/estudiantes`;
-   
-
-    const [estudiantes, setEstudiantes] = useState([]);
+    
     const [matriculas, setMatriculas] = useState([]);
-   
+
     useEffect(() => {
-        cargarEstudiante()
         cargarMatricula()
+   
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    const cargarEstudiante = async () => {//peticion asicrona
+
+   /*const cargarMatricula = async () => {//peticion asicrona
         try {
-           
-            const resultado = await axios.get(urlBase);
-           
-            if (resultado ) {
-                // console.log(resultado.data);
-                setEstudiantes(resultado.data);
-                
+            const resulMatricula = await axios.get(urlMatricula);
+            if (resulMatricula) {
+                const m = resulMatricula.data.map((i) => {
+
+                    return { idmatricula: i.idmatricula }
+                })
+                console.log(m);
+
+                setMatriculas(m)
             }
         } catch (error) { }
-    }
-
+    }*/
     const cargarMatricula = async () => {//peticion asicrona
         try {
             const resulMatricula = await axios.get(urlMatricula);
-            if ( resulMatricula) {
-                // console.log(resultado.data);
-                
-                setMatriculas(resulMatricula.data)
+            if (resulMatricula) {
+                 console.log(resulMatricula.data);
+                setMatriculas(resulMatricula.data);
             }
         } catch (error) { }
     }
 
-
-    const onClickDelete = async (id) => {
+   /*const onClickDelete = async (id) => {
         try {
             const respuesta = await axios
                 .delete(`${urlBase}/${id}`);
@@ -56,30 +52,30 @@ export default function ListMatriculas() {
         } catch (error) {
             console.log(error)
         }
-    }
+    }*/
 
     const onFilter = (data) => {
         console.log('Entro')
-        setEstudiantes(data);
+        //setEstudiantes(data);
     };
 
     const renderHeader = () => {
 
         return (
 
-           /* <div className="busqueda">
-                 <BuscarEstudiantes 
-                 onFilter={onFilter} loadAll={cargarEstudiante}/>
-            
-                <div className="btnAdd">
-                    <Button className='btn-add'
-                        raised label=" Nuevo "
-                        icon="pi pi-plus"
-                       // onClick={() => setshowAddMode(true)}
-                    />
-                </div>
+            /* <div className="busqueda">
+                  <BuscarEstudiantes 
+                  onFilter={onFilter} loadAll={cargarEstudiante}/>
              
-            </div>*/
+                 <div className="btnAdd">
+                     <Button className='btn-add'
+                         raised label=" Nuevo "
+                         icon="pi pi-plus"
+                        // onClick={() => setshowAddMode(true)}
+                     />
+                 </div>
+              
+             </div>*/
             <h1>probando cambios</h1>
         );
     };
@@ -90,20 +86,20 @@ export default function ListMatriculas() {
         return (
             <>
                 <button className='btn btn-success' onClick={() => {
-                   // setSelectEstudianteID(RowDate.idpersona)
-                   // setshowViewMode(true)
+                    // setSelectEstudianteID(RowDate.idpersona)
+                    // setshowViewMode(true)
                 }} >
                     <img className='icon' src="https://img.icons8.com/material-outlined/visible--v1.png" alt="visible--v1" />
                 </button>
                 <button className='btn btn-primary'
                     onClick={() => {
                         //setSelectEstudianteID(RowDate.idpersona)
-                       // setshowEditMode(true)
+                        // setshowEditMode(true)
                     }}  >
                     <img className='icon' src="https://img.icons8.com/material-outlined/edit--v1.png" alt="edit--v1" />
                 </button>
                 <button className='btn btn-danger' onClick={() => {
-                    onClickDelete(RowDate.idpersona);
+                   // onClickDelete(RowDate.idpersona);
                 }}  >
                     <img className='icon' src="https://img.icons8.com/material-outlined/filled-trash.png" alt="filled-trash" />
                 </button>
@@ -116,20 +112,25 @@ export default function ListMatriculas() {
         <div className="card">
             <Card title="Matriculas">
 
-                <DataTable  paginator rows={10} dataKey="id" stripedRows
+                <DataTable  value={matriculas}  paginator rows={10} stripedRows dataKey="id"
                     //  filters={filters} filterDisplay="row" loading={loading}
                     header={header} emptyMessage="No se encontro el estudiante."
                 >
-                    <Column field="idmatricula" header="ID" style={{ minWidth: '4rem' }} value={matriculas} />
-                    <Column field="nombre_completo" header="Nombres" style={{ minWidth: '12rem' }} valu={estudiantes} />
+                   
+                    <Column field="idmatricula" header="ID Matricula" style={{ minWidth: '10rem' }} />
+                    <Column value={matriculas.idGrado} header="ID Matricula" style={{ minWidth: '10rem' }} />
+                    <Column field="costo_matricula" header="Turno" style={{ minWidth: '12rem' }} />
                     <Column field="turno" header="Turno" style={{ minWidth: '12rem' }} />
-                    <Column field="costo_matricula" header="Costo" style={{ minWidth: '12rem' }} />
-                    <Column field="estado" header="Estado" style={{ minWidth: '10rem' }} />
                     <Column header="Accion" body={actionsTemplate} style={{ minWidth: '12rem' }} ></Column>
-                </DataTable>
+                </DataTable> 
+
             </Card>
-         
+
 
         </div>
     );
 }
+
+/*<Column field="turno" header="Turno" style={{ minWidth: '12rem' }} />
+                    <Column field="costo_matricula" header="Costo" style={{ minWidth: '12rem' }} />
+                    */
